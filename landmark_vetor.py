f376f585db28aaebb39823768b457d86838e4cfb
Breno <brenocal73@gmail.com>
2024-12-15 19:27:44 -0300

    feat: Mais dados nos vetores.

---
+++ landmark_vetor.py
@@ -0,0 +1,8 @@
+def landmark_vetor(landmark):
+  lista = []
+  for item in landmark.landmark:
+    lands = [item.x, item.y, item.z]
+    lista.append(lands)
+      
+      
+  return lista
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import mediapipe as mp
 import math
 
 import letters_numbers as ln
+import landmark_vetor as lv
 
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1080)
@@ -13,14 +14,16 @@ mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 hand = mp_hands.Hands(max_num_hands=1)
 
+count = 0
+lista = []
 while True:
   success, frame = cap.read()
 
   if success:
     RGB_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     result = hand.process(RGB_frame)
-    if result.multi_hand_landmarks:
-      min_distance = 1000
+    if result.multi_hand_world_landmarks:
+      """ min_distance = 1000
       letter = ""
       handLandmarks = result.multi_hand_landmarks[0]
       for chave, valor in ln.vetor.items():
@@ -34,15 +37,20 @@ while True:
           min_distance = soma
           letter = chave
           
-      print(letter)
+      print(letter) """
           
-      #for hand_landmarks in result.multi_hand_landmarks:
+      for hand_landmarks in result.multi_hand_world_landmarks:
         #print(hand_landmarks)
-        #mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
       
     cv2.imshow("capture image", frame)
     if cv2.waitKey(1) == ord('q'):
       #print(hand_landmarks)
+      print(lista)
       break
+    elif cv2.waitKey(1) == ord('p'):
+      count += 1
+      print(count)
+      lista.append(lv.landmark_vetor(hand_landmarks))
     
 cv2.destroyAllWindows
